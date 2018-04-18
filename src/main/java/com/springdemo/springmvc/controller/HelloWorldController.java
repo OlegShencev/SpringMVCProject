6258447e3bd4870dad1832bae2b5b5c6b6de40bc
OlegShencev <olegsh91@gmail.com>
2018-04-18 12:17:25 +0300

    Resolved problem with Russian symbols

--- src/main/java/com/springdemo/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/springdemo/springmvc/controller/HelloWorldController.java
@@ -1,17 +1,20 @@
 package com.springdemo.springmvc.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.awt.*;
+
 @Controller
 public class HelloWorldController {
 
-	@GetMapping("/")
+	@GetMapping(value = "/",produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
 	public String sayHello(ModelMap model) {
-		model.addAttribute("helloworld", "Hello World!");
+		model.addAttribute("helloworld", "Здравствуй мир!");
 		return "welcome";
 	}
 }
