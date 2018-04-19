29cbc89a1768bcf0ee44a150f3a77d6c9a9abb18
OlegShencev <olegsh91@gmail.com>
2018-04-19 20:51:18 +0300

    Removed uselles import and MediaType.APPLICATION_JSON_VALUE

--- src/main/java/com/springdemo/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/springdemo/springmvc/controller/HelloWorldController.java
@@ -1,6 +1,5 @@
 package com.springdemo.springmvc.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,7 @@ import java.awt.*;
 @Controller
 public class HelloWorldController {
 
-	@GetMapping(value = "/",produces = MediaType.APPLICATION_JSON_VALUE + "; charset=utf-8")
+	@GetMapping(value = "/")
 	public String sayHello(ModelMap model) {
 		model.addAttribute("helloworld", "Здравствуй мир!");
 		return "welcome";
