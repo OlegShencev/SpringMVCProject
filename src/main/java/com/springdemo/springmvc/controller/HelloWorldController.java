eef1223d4f20fadc38f34dbf4f9b1653f7f2b4b8
OlegShencev <olegsh91@gmail.com>
2018-04-19 21:08:11 +0300

    Removed import java.awt.*

--- src/main/java/com/springdemo/springmvc/controller/HelloWorldController.java
+++ src/main/java/com/springdemo/springmvc/controller/HelloWorldController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.awt.*;
 
 @Controller
 public class HelloWorldController {
